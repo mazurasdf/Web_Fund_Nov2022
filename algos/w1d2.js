007cdd31813efd9c103a88297da22097af7580f7
mazurasdf <mazurasdf@gmail.com>
2022-11-08 11:04:54 -0600

    w1d2 algo file

---
+++ algos/w1d2.js
@@ -0,0 +1,27 @@
+// var a = 25;
+// a = a - 13;
+// console.log(a/2);
+// console.log(a);
+
+// a = "hello";
+// console.log(a + " hello");
+// console.log(a);
+
+// for(var i = 0; i < 10; i++){
+//     console.log(i);
+//     i = i + 3;
+// }
+// console.log("outside the loop " + i);
+
+// function getTotal(arrayOfNumbers){
+//     var sum = arrayOfNumbers[0];
+
+//     for(var i = 0; i < arrayOfNumbers.length; i++){
+//         sum += arrayOfNumbers[i];
+//         console.log("the current sum is: " + sum);
+//     }
+
+//     console.log("the total is: " + sum);
+// }
+
+// getTotal([1,3,5]);
