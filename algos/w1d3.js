2784bbf1ba25ad577fbf485ef0b9b9c3e4c9ffdc
mazurasdf <mazurasdf@gmail.com>
2022-11-09 12:18:59 -0600

    morning for week 1 day 3

---
+++ algos/w1d3.js
@@ -0,0 +1,38 @@
+// var isSunny = true;
+// var temp = 45;
+// var isRaining = false;
+// var whatToBring = "I should bring: ";
+
+// if(isSunny){
+//     whatToBring += "sunglasses, ";
+// }
+
+// if(temp < 50){
+//     whatToBring += "a jacket, ";
+// }
+
+// if(isRaining){
+//     whatToBring += "umbrella, ";
+// }
+
+// whatToBring += "and a smile!";
+
+// console.log(whatToBring);
+
+// for(var i = 10; i > 0; i--){
+//     if(i !== 2){
+//         console.log(i);
+//     }
+//     else{
+//         console.log("ignition!");
+//     }
+// }
+
+// console.log("liftoff!");
+
+var countPositives = 0;
+var numbers = [3, 4, -2, 7, 16, -8, 0];
+    
+// your code here!
+    
+console.log("there are " + countPositives + " positive values");
